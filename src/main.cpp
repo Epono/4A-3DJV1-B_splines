5bd8514ad5d0632628449af1ffcebde0fad7f75d
Epono <guillaume.ambrois@gmail.com>
2015-05-11 16:00:19 +0200

    Un peu des trucs

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,8 @@
 /*******************************************************/
 #include "../headers/utils.h"
 
+#include <vector>
+
 #include <iostream>
 #include <windows.h>
 #include <GL/glut.h>
@@ -20,8 +22,12 @@
 int creationState = waitingForFirstClick;
 
 CustomPolygon window;						// Window used to cut another polygon
+CustomPolygon windows[256];
+int windowsCount;
 float windowColor[3] = {0, 0.5f, 0.5f};		// Window color
 int windowVerticeToMove = -1;
+bool hideControlPoints = false;
+float step = 0.01;
 
 int presse = 0;								// Stores if the mouse is dragging
 /* Functions prototypes */
@@ -37,6 +43,11 @@ void menu(int opt);
 void colorPicking(int option);
 void setPolygonColor(float colors[3], float r, float g, float b);
 void write();										// Writes on the top left what's happening
+
+void decasteljau();
+Point drawBezier(Point A, Point B, Point C, Point D, double t);
+void drawLine(Point p1, Point p2);
+
 // Debug
 void printPolygon(int polygonCount);
 
@@ -44,14 +55,14 @@ int main(int argc, char **argv) {
 	//Glut and Window Initialization
 	glutInit(&argc, argv);										// Initializes Glut
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);	// RGB display mode, with depth
-	glutInitWindowSize(500, 500);								// Sets window's dimensions
+	glutInitWindowSize(1280, 720);								// Sets window's dimensions
 	glutInitWindowPosition(100, 100);							// Positions the window
 	glutCreateWindow("FunStuffWithOpenGL");						// Title of the window
 
-	gluOrtho2D(0, 500, 500, 0);									// 2D orthogonal frame with xMin, xMax, yMin, yMax
+	gluOrtho2D(0, 1280, 720, 0);									// 2D orthogonal frame with xMin, xMax, yMin, yMax
 
 	// OpenGL initialization
-	glClearColor(1.0, 1.0, 1.0, 1.0);		// Background color : black ?
+	glClearColor(0.0, 0.0, 0.0, 1.0);		// Background color : black ?
 	glColor3f(0.0, 0.0, 0.0);				// Color : white
 	glPointSize(4.0);						// Point size : 4px
 
@@ -69,6 +80,32 @@ int main(int argc, char **argv) {
 	return 0;				// Should not get here (unless we exit with 'q' ?)
 }
 
+
+//void DeCasteljau(float t, std::vector<Point> points, Point &res) {
+//	std::vector<Point> barycentres(points.size());
+//
+//	for(int i = 0; i != points.size(); ++i) {
+//		barycentres[i] = 
+//	}
+//
+//	for(int j = 1; j != points.size(); ++j) {
+//		for(int i = 1; i != points.size() - j; ++i) {
+//
+//		}
+//	}
+//}
+
+void decasteljau() {
+	Point POld = window.vertices[0];
+	for(int i = 0; i < window.nbVertices - 3; ++i) {
+		for(double t = 0.0; t <= 1.0; t += step) {
+			Point P = drawBezier(window.vertices[i], window.vertices[i + 1], window.vertices[i + 2], window.vertices[i + 3], t);
+			drawLine(POld, P);
+			POld = P;
+		}
+	}
+}
+
 Point drawBezier(Point A, Point B, Point C, Point D, double t) {
 	Point P;
 
@@ -82,18 +119,7 @@ void drawLine(Point p1, Point p2) {
 	glBegin(GL_LINES);
 	glVertex2i(p1.x, p1.y);
 	glVertex2i(p2.x, p2.y);
-
 	glEnd();
-}
-
-void decasteljau() {
-	Point POld = window.vertices[0];
-	for(double t = 0.0; t <= 1.0; t += 0.01) {
-		Point P = drawBezier(window.vertices[0], window.vertices[1], window.vertices[2], window.vertices[3], t);
-		printf("%d - %d\n", P.x, P.y);
-		drawLine(POld, P);
-		POld = P;
-	}
 }
 
 /*
@@ -194,6 +220,22 @@ void keyboard(unsigned char key, int x, int y) {
 			creationState = selectPoint;
 		}
 		break;
+	case 'h':
+		// hide control points
+		hideControlPoints = !hideControlPoints;
+		break;
+	case '-':
+		// increase step
+		if(step + 0.01 < 1) {
+			step += 0.01;
+		}
+		break;
+	case '+':
+		// decrease step
+		if(step - 0.01 > 0) {
+			step -= 0.01;
+		}
+		break;
 	case 127:
 		// deletes selected point
 		if(windowVerticeToMove != -1) {
@@ -216,14 +258,40 @@ void keyboard(unsigned char key, int x, int y) {
 	glutPostRedisplay(); // Rafraichissement de l'affichage
 }
 
+void colorPicking(int option) {
+	switch(option) {
+	case 0:
+		printf("Vert\n");
+		setPolygonColor(windowColor, 0.f, 1.f, 0.f);
+		break;
+	case 1:
+		printf("Bleu\n");
+		setPolygonColor(windowColor, 0.f, 0.f, 1.f);
+		break;
+	case 2:
+		printf("Rouge\n");
+		setPolygonColor(windowColor, 1.f, 0.f, 0.f);
+		break;
+	default:
+		break;
+	}
+	display();
+}
+
 //TODO
 /*
 * Creates the menu available via right-click
 */
 void createMenu() {
-	int mainMenu;
+	int mainMenu, colorMenu;
+
+	colorMenu = glutCreateMenu(colorPicking);
+	glutAddMenuEntry("Vert", 0);
+	glutAddMenuEntry("Bleu", 1);
+	glutAddMenuEntry("Rouge", 2);
 
 	mainMenu = glutCreateMenu(menu);
+	glutAddSubMenu("Couleurs", colorMenu);
 	glutAddMenuEntry("Polygone (tracer)", 1);
 	glutAddMenuEntry("Fenetre (tracer)", 2);
 
@@ -253,33 +321,35 @@ void setPolygonColor(float colors[3], float r, float g, float b) {
 }
 
 void drawPolygon(CustomPolygon cp, float color[], int lineSize) {
-	if(window.nbVertices == 4) {
+	if(window.nbVertices >= 4) {
 		decasteljau();
 	}
 
-	// Draws vertices of the connected lines strip
-	glBegin(GL_POINTS);
-	for(int j = 0; j < cp.nbVertices; j++) {
-		glVertex2i(cp.vertices[j].x, cp.vertices[j].y);
-	}
-	glEnd();
-
-	// Draw selected point bigger
-	if(windowVerticeToMove != -1) {
-		glPointSize(6.0);
+	if(!hideControlPoints) {
+		// Draws vertices of the connected lines strip
 		glBegin(GL_POINTS);
-		glVertex2i(cp.vertices[windowVerticeToMove].x, cp.vertices[windowVerticeToMove].y);
+		for(int j = 0; j < cp.nbVertices; j++) {
+			glVertex2i(cp.vertices[j].x, cp.vertices[j].y);
+		}
 		glEnd();
-		glPointSize(4.0);
-	}
 
-	glLineWidth(lineSize);
-	// Draws the polygon
-	glColor3fv(color);
-	glBegin(GL_LINE_STRIP);
-	for(int j = 0; j < cp.nbVertices; j++)
-		glVertex2i(cp.vertices[j].x, cp.vertices[j].y);
-	glEnd();
+		// Draw selected point bigger
+		if(windowVerticeToMove != -1) {
+			glPointSize(6.0);
+			glBegin(GL_POINTS);
+			glVertex2i(cp.vertices[windowVerticeToMove].x, cp.vertices[windowVerticeToMove].y);
+			glEnd();
+			glPointSize(4.0);
+		}
+
+		glLineWidth(lineSize);
+		// Draws the polygon
+		glColor3fv(color);
+		glBegin(GL_LINE_STRIP);
+		for(int j = 0; j < cp.nbVertices; j++)
+			glVertex2i(cp.vertices[j].x, cp.vertices[j].y);
+		glEnd();
+	}
 }
 
 void write() {
@@ -298,4 +368,3 @@ void write() {
 	for(int i = 0; truc[i] != '\0'; i++)
 		glutBitmapCharacter(GLUT_BITMAP_9_BY_15, truc[i]);
 }
-
