86069756162ccb107287af32495d91d89d3434a1
Epono <guillaume.ambrois@gmail.com>
2015-05-13 12:50:04 +0200

    Ouais en fait nan ca plante

--- Splines/headers/utils.h
+++ Splines/headers/utils.h
@@ -17,27 +17,3 @@ typedef struct color_rgb {
 	float _g;
 	float _b;
 } color_rgb;
-
-/* Functions prototypes */
-void display();										// manages displaying
-void keyboard(unsigned char key, int x, int y);		// manages keyboard inputs
-void keyboardSpecial(int key, int x, int y);		// manages keyboard inputs
-void mouse(int bouton, int etat, int x, int y);		// manages mouse clicks
-void motion(int x, int y);							// manages mouse motions
-
-void drawWindow();									// draws the window (algorithm of my bite)
-void createMenu();
-void menu(int opt);
-void colorPicking(int option);
-void setPolygonColor(float colors[3], float r, float g, float b);
-void write();										// Writes on the top left what's happening
-
-Point* DC(const std::vector<Point>& p, float t);
-void drawBezier(int p, LineStrip& line);
-Point drawBezier(Point A, Point B, Point C, Point D, double t);
-void drawLine(Point& p1, Point& p2);
-void drawCurve(LineStrip& line, int lineSize);
-
-void translate(int xOffset, int yOffset);
-void scale(float scaleX, float scaleY);
-void rotate(float angle);
--- Splines/src/main.cpp
+++ Splines/src/main.cpp
@@ -23,6 +23,30 @@ color_rgb dessinColor = color_rgb(1.f, 0.f, 0.f);
 
 int presse = 0;										// Stores if the mouse is dragging
 
+/* Functions prototypes */
+void display();										// manages displaying
+void keyboard(unsigned char key, int x, int y);		// manages keyboard inputs
+void keyboardSpecial(int key, int x, int y);		// manages keyboard inputs
+void mouse(int bouton, int etat, int x, int y);		// manages mouse clicks
+void motion(int x, int y);							// manages mouse motions
+
+void drawWindow();									// draws the window (algorithm of my bite)
+void createMenu();
+void menu(int opt);
+void colorPicking(int option);
+void setPolygonColor(float colors[3], float r, float g, float b);
+void write();										// Writes on the top left what's happening
+
+Point* DC(const std::vector<Point>& p, float t);
+void drawBezier(int p, LineStrip& line);
+Point drawBezier(Point A, Point B, Point C, Point D, double t);
+void drawLine(Point& p1, Point& p2);
+void drawCurve(LineStrip& line, int lineSize);
+
+void translate(int xOffset, int yOffset);
+void scale(float scaleX, float scaleY);
+void rotate(float angle);
+
 int main(int argc, char **argv) {
 	//Glut and Window Initialization
 	glutInit(&argc, argv);										// Initializes Glut
